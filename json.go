5c6167b663f9857ee68884e556239a6aa27b63f8
lumifish <eni.lume@gmail.com>
2024-10-02 09:28:37 +0200

    fix gosec issues

--- json.go
+++ json.go
@@ -23,9 +23,16 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	dat, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
+		w.WriteHeader(http.StatusInternalServerError) // Use http.StatusInternalServerError instead of 500
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	
+	// Handle potential error from w.Write()
+	_, err = w.Write(dat)
+	if err != nil {
+		// Log the error, but we can't really recover at this point
+		log.Printf("Error writing response: %v", err)
+		// Note: We've already sent headers, so we can't change the status code here
+	}
 }
